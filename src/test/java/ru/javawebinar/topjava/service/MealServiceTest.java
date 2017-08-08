9596dc1adfc37db6a9367a28abdcd27b6f9ac75b
Olena <olena.kramarenko@gmail.com>
2017-08-08 17:06:38 +0300

    myHW03-optional-tests

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -7,7 +7,17 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -33,36 +43,53 @@ public class MealServiceTest {
 
     @Test
     public void testGet() throws Exception {
+        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+        MATCHER.assertEquals(ADMIN_MEAL1, actual);
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception{
+        service.get(MEAL1_ID, ADMIN_ID);
     }
 
     @Test
     public void testDelete() throws Exception {
+        service.delete(MEAL1_ID, USER_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), service.getAll(USER_ID));
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        service.delete(MEAL1_ID, ADMIN_ID);
     }
 
     @Test
-    public void testGetBetweenDates() throws Exception {
-
-    }
-
-    @Test
-    public void testGetBetweenDateTimes() throws Exception {
-
+    public void testGetBetween() throws Exception {
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL3, MEAL2, MEAL1),
+                service.getBetweenDates(
+                        LocalDate.of(2015, Month.MAY, 30),
+                        LocalDate.of(2015, Month.MAY, 30), USER_ID));
     }
 
     @Test
     public void testGetAll() throws Exception {
-
+        MATCHER.assertCollectionEquals(MEALS, service.getAll(USER_ID));
     }
 
     @Test
     public void testUpdate() throws Exception {
+        MATCHER.assertEquals(getUpdated(), service.update(getUpdated(), USER_ID));
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testUpdateNotFound() throws Exception {
+        service.update(getUpdated(), ADMIN_ID);
     }
 
     @Test
     public void testSave() throws Exception {
-
+        Meal created = getCreated();
+        service.save(created, USER_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(created, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), service.getAll(USER_ID));
     }
 }
